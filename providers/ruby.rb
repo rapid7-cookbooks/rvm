b7860d26bd1fa867bd38d797a65a1d9b92240d6a
Chris Doughty <chris_doughty@rapid7.com>
2018-05-24 14:49:56 -0500

    PD-16292 Patch to allow running on Ubuntu >14.04

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -130,10 +130,18 @@ def install_ruby_dependencies(rubie)
   when /^ruby-/, /^ree-/, /^rbx-/, /^kiji/
     case node['platform']
       when "debian","ubuntu"
-        pkgs  = %w{ build-essential openssl libreadline6 libreadline6-dev
-                    zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev
-                    sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
-                    ncurses-dev automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
+        pkgs = nil
+        if Chef::VersionConstraint.new("> 14.04").include?(node['platform_version'])
+          pkgs  = %w{ build-essential openssl libreadline-dev
+                      zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev
+                      sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
+                      ncurses-dev automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
+        else
+          pkgs  = %w{ build-essential openssl libreadline6 libreadline6-dev
+                      zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev
+                      sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
+                      ncurses-dev automake libtool bison ssl-cert pkg-config libgdbm-dev libffi-dev}
+        end
         pkgs += %w{ subversion }  if rubie =~ /^ruby-head$/
       when "suse"
         pkgs = %w{ gcc-c++ patch zlib zlib-devel libffi-devel
